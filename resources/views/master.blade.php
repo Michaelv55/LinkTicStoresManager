c2b69252f337a399dadaf82c1bd875117b41019b
Michael Velasco <maicolvelasco55@gmail.com>
2021-08-09 14:10:01 -0500

    completar assets para la vista maestra

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,10 +7,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>LinkTic Stores Manager</title>
     <link rel="stylesheet" href={{ asset('css/app.css') }}>
+    <link rel="stylesheet" href={{ asset('js/app.js') }}>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container-fluid m-0 p-0">
         @component('master-components.header')@endcomponent
+        <div class="container mt-5" id="main-contaier">
+            @yield('content')
+        </div>
     </div>
 </body>
 </html>
