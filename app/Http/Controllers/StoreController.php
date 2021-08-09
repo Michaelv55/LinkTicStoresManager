dbfe6a640d75b915cbd27106599813093d7c9c8d
Michael Velasco <maicolvelasco55@gmail.com>
2021-08-09 12:13:33 -0500

    creación de primera ruta del api, productos por tienda

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -18,16 +18,6 @@ class StoreController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -46,18 +36,7 @@ class StoreController extends Controller
      */
     public function show(Store $store)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Store  $store
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Store $store)
-    {
-        //
+        return $store->products;
     }
 
     /**
